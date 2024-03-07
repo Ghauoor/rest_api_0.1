57b37e0f03e3404c1e054a2ffeab4ed769dd5a9d
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-03-08 00:57:41 +0500

    Search and Sorting in Document

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,30 @@
 const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-  const { company, name } = req.query;
+  const { company, name, featured, sort } = req.query;
   const queryObject = {};
 
   if (company) {
     queryObject.company = company;
   }
-  if (name) {
-    queryObject.name = name;
+  if (featured) {
+    queryObject.featured = featured;
   }
-  console.log(queryObject);
-  const myData = await Product.find(queryObject);
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let apiData = Product.find(queryObject);
+
+  if (sort) {
+    let sortFix = sort.replace(",", " ");
+    // queryObject.sort = sortFix;
+    apiData = apiData.sort(sortFix);
+  }
+  const myData = await apiData;
   res.status(200).json({ myData });
 };
+
 const getAllProductsTesting = async (req, res) => {
   const myData = await Product.find(req.query);
   res.status(200).json(myData);
