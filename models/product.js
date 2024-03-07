0516b94f1cbb5024022756d1bfe9dd7f9884bfa4
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-03-07 13:32:18 +0500

    Model for Product

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const { type } = require("express/lib/response");
+const mongooes = require("mongoose");
+
+const productSchema = new mongooes.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price Must be Added"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date(),
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["apple", "msi", "samsung", "dell"],
+      message: `{VALUE} is not supported`,
+    },
+  },
+});
+
+module.exports = mongooes.model("Product", productSchema);
