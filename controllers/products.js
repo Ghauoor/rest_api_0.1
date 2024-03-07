87f7f9d7eacec663796723b323e4ad8ca9c32b3c
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-03-07 23:55:50 +0500

    Searching in Doc

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,22 @@
+const Product = require("../models/product");
+
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "i'm getting all the products" });
+  const { company, name } = req.query;
+  const queryObject = {};
+
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = name;
+  }
+  console.log(queryObject);
+  const myData = await Product.find(queryObject);
+  res.status(200).json({ myData });
 };
 const getAllProductsTesting = async (req, res) => {
-  res.status(200).json({ msg: "i'm getting all the products Testing" });
+  const myData = await Product.find(req.query);
+  res.status(200).json(myData);
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
